84ab97b5b79eb30b6fd6970b42ac2d9663bc9a77
Abdulrahman <abdulrahmanabusamra0@gmail.com>
2024-12-18 16:56:48 +0200

    style mobile

--- JavaScript/javascript.js
+++ JavaScript/javascript.js
@@ -1,3 +1,22 @@
+const hamburgerIcon = document.querySelector('.fa-bars');
+const navMenu = document.querySelector('nav ul');
+
+hamburgerIcon.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
+
+document.addEventListener('click', (event) => {
+  // التحقق ما إذا كان العنصر الذي تم النقر عليه ليس جزءًا من القائمة
+  if (!navMenu.contains(event.target) && navMenu.classList.contains('active') && event.target !== hamburgerIcon) {
+    navMenu.classList.remove('active');
+  }
+});
+
+
+
+
+
+
 let section = document.querySelector(".section-4")
 let spans = document.querySelectorAll(".line-out  span")
 
